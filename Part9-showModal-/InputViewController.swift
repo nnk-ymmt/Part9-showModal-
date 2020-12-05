20cb29d7426aadc5700aad0ec08f8f9aee52bd4f
nnk-ymmt <garden.n1234567890@gmail.com>
2020-12-05 23:13:14 +0900

    Initial commit

---
+++ Part9-showModal-/InputViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InputViewController.swift
+//  Part9-showModal-
+//
+//  Created by 山本ののか on 2020/12/05.
+//
+
+import UIKit
+
+final class InputViewController: UIViewController {
+
+    @IBAction private func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Part9-showModal-/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Part9-showModal-
+//
+//  Created by 山本ののか on 2020/12/05.
+//
+
+import UIKit
+
+enum Prefecture {
+    case tokyo
+    case saitama
+    case chiba
+
+    var displayName: String {
+        switch self {
+        case .tokyo:
+            return "東京都"
+        case .saitama:
+            return "埼玉県"
+        case .chiba:
+            return "千葉県"
+        }
+    }
+}
+
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var prefectureLabel: UILabel!
+
+    var prefecture: Prefecture?
+
+    @IBAction private func selectTokyo(segue: UIStoryboardSegue) {
+        prefecture = .tokyo
+        displayPrefecture()
+    }
+
+    @IBAction private func selectSaitama(segue: UIStoryboardSegue) {
+        prefecture = .saitama
+        displayPrefecture()
+    }
+
+    @IBAction private func selectChiba(segue: UIStoryboardSegue) {
+        prefecture = .chiba
+        displayPrefecture()
+    }
+
+    func displayPrefecture() {
+        prefectureLabel.text = prefecture?.displayName
+    }
+}
